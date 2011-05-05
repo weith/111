425985cf44e0f0e5066195fc37d3d3e76a30b618
weith <weith_007@sina.com>
2011-05-06 01:41:04 +0800

    Holding invalid signin

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,11 +23,10 @@ class User < ActiveRecord::Base
   end
 
  class << self
-  def self.authenticate(email, submitted_password)
-    user = find_by_email(email)
-    return nil  if user.nil?
-    return user if user.has_password?(submitted_password)
-  end
+    def authenticate(email, submitted_password)
+       user = find_by_email(email)
+      (user && user.has_password?(submitted_password)) ? user : nil
+    end
  end
   private
     def encrypt_password
