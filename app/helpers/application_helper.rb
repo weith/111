5a1a5ac497d042e82338900a4e99d6b6e564ac66
weith <weith_007@sina.com>
2011-05-06 15:10:14 +0800

    Combination

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,15 @@
+module ApplicationHelper
+  #retune a title on a per-page basis.
+  def title
+    base_title = "Ruby on Rails Weith Sample App"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+
+  def logo
+    image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '202d43fa93ca98570e9256749505f4775ffee7fc5b396e5694eaded15f26efb47c9816ba4a78fc67e80240746c6cdd7cbfe12340095d35617f6e11a315a41beb'
