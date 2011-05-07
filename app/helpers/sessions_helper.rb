41278423b61644fb9dda1f0c9a085bf2499b9765
weith <weith_007@sina.com>
2011-05-07 14:22:29 +0800

    Completed signin/out

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,8 @@
 module SessionsHelper
+
   def sign_in(user)
     cookies.permanent.signed[:remember_token] = [user.id, user.salt]
-    current_user = user
+    self.current_user = user
   end
 
   def current_user=(user)
@@ -12,17 +13,23 @@ module SessionsHelper
     @current_user ||= user_from_remember_token
   end
 
-  def sign_in?
+  def signed_in?
     !current_user.nil?
+  end
+
+  def sign_out
+    cookies.delete(:remember_token)
+    self.current_user = nil
   end
 
   private
   def user_from_remember_token
     User.authenticate_with_salt(*remember_token)
   end
-  
+
   def remember_token
     cookies.signed[:remember_token] || [nil, nil]
   end
+  
 end
 
